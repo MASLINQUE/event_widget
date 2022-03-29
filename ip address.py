86221c67711a5351bdef74184ab63b6c6e3ca4d5
trunov <trunov@enterideas.com>
2022-03-29 17:57:30 +0300

    dele

--- ip address.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python3
-
-import json
-import requests
-
-resp = requests.get('https://ipinfo.io').text
-ipinfo = json.loads(resp)
-
-print("<div class='stat'><h3>"+ipinfo["ip"]+"</h3><span class='subtitle'>"+ipinfo["city"]+"</span></div>")
